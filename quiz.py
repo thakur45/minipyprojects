830eb61c51b4a322a4bf77982fdd9325a7436bf9
Mayuresh Thakur <104351643+thakur45@users.noreply.github.com>
2022-10-18 17:10:26 +0530

    Added questions and percentage

--- quiz.py
+++ quiz.py
@@ -15,5 +15,35 @@ if answer.lower() == "central processing unit":
     score += 1
 else:
     print("Incorrect")
+
+answer = input("What does RAM stands for ? ")
+if answer.lower() == "random access memory":
+    print("Correct !")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does ROM stands for ? ")
+if answer.lower() == "read only memory":
+    print("Correct !")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does GPU stands for ? ")
+if answer.lower() == "graphic processing unit":
+    print("Correct !")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does WWW stands for ? ")
+if answer.lower() == "world wide web":
+    print("Correct !")
+    score += 1
+else:
+    print("Incorrect")
+
     
 print("Your score is " + str(score))
+print("Your " + str((score/5)*100) + "%" " of answers correct")
